7c721e0960e3734977a6c4fef73eb72147248007
wiktor779 <wiktor.bieda@gmail.com>
2018-03-21 23:01:40 +0100

    changed name from make_move to try_make_move

--- tests.py
+++ tests.py
@@ -8,16 +8,16 @@ class TestExample(unittest.TestCase):
         self.assertTrue(True)
 
 class TestModel(unittest.TestCase):
-    def test_make_move(self):
+    def test_try_make_move(self):
         a = model.Board()
-        self.assertTrue(a.make_move(1, 0, 0))
-        self.assertFalse(a.make_move(2, 0, 0))
-        self.assertTrue(a.make_move(1, 2, 2))
-        self.assertFalse(a.make_move(1, 3, 3))
+        self.assertTrue(a.try_make_move(1, 0, 0))
+        self.assertFalse(a.try_make_move(2, 0, 0))
+        self.assertTrue(a.try_make_move(1, 2, 2))
+        self.assertFalse(a.try_make_move(1, 3, 3))
     def test_get_board(self):
         a = model.Board()
-        a.make_move(1, 0, 0)
-        a.make_move(2, 1, 0)
+        a.try_make_move(1, 0, 0)
+        a.try_make_move(2, 1, 0)
         result = [[1, 0, 0], [2, 0, 0], [0]*3] == a.get_board()
         self.assertTrue(result)
 
